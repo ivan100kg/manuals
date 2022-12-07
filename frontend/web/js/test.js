94acefa5a35c00be7be5a063c418034553bb8d90
IvanB <ivar110kg@gmail.com>
2022-12-07 20:19:45 +0400

    Add tests

--- frontend/web/js/test.js
+++ frontend/web/js/test.js
@@ -1,11 +1,17 @@
-describe("pow", function() {                    // описание тестир ф-ии
-    console.log(her);
-    it("возводит 2 степень 3", function() {     // тесты(1+)
-        assert.equal(pow(2, 3), 8);
+describe("pow", function() {
+    before(() => console.log("перед всеми"));
+    after(() => console.log("после всех"));
+    beforeEach(() => console.log("перед каждым"));
+    afterEach(() => console.log("после каждого"));
+    
+    it("2 в степени 3 будет 8", () => assert.equal(pow(2, 3), 8));
+    it("3 в степени 3 будет 27", function() {
+        assert.equal(pow(3, 3), 27);
     });
-
-    it("возводит 3 степень 11", function() {     // тесты(1+)
-        assert.equal(pow(3, 11), 177147);
+    it("для отрицательных n возвращает NaN", function() {
+        assert.isNaN(pow(2, -1));
     });
-
+    it("для дробных n возвращает NaN", function() {
+        assert.isNaN(pow(2, 1.5));
+    });
 });
