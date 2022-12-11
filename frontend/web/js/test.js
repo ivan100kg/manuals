423143717462080c75fecf557d1256066dbd9510
IvanB <ivar110kg@gmail.com>
2022-12-11 22:10:56 +0400

    Learn js

--- frontend/web/js/test.js
+++ frontend/web/js/test.js
@@ -24,3 +24,20 @@ user.surname = 'Smith';
 user.name = 'Pete';
 delete user.name;
 console.log(user);
+
+let calculator = {
+    read(a, b) {
+        this.a = a;
+        this.b = b; 
+    },
+    sum() {
+        return this.a + this.b;
+    },
+    mul() {
+        return this.a * this.b;
+    }
+}
+
+calculator.read(2,3);
+console.log(calculator.sum());
+console.log(calculator.mul());
