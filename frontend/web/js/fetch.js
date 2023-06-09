d43229acefc51fae4647bc3cff4bfeecdd6a88f3
IvanB <ivar110kg@gmail.com>
2023-06-09 12:18:19 +0400

    Add system info

--- frontend/web/js/fetch.js
+++ frontend/web/js/fetch.js
@@ -4,20 +4,13 @@ const url = 'https://httpbin.org/get';
 
 console.log('hello 1');
 
-// asyncGetFetch(url);
-// syncGetFetch(url);
+getFetch(url);
 
 console.log('hello 2');
 
-function asyncGetFetch(url) {
-    fetch(url)                                          // url
-        .then(response => response.json())              // ответ в JSON ковертируем в объект
-        .then(obj => console.log('async ' + obj.origin))  // обрабатываем дальше наш объект
-        .catch(err => console.log('Fuck!' + err));      // в случае ошибки
-}
 
-async function syncGetFetch(url) {
+async function getFetch(url) {
     const responce = await fetch(url);
     const data = await responce.json();
-    console.log('sync ' + data);
+    console.log(data.origin);
 }
