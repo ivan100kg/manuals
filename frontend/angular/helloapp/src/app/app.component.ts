ea48d47137c39838d116d67411717ee22a16df9d
ivanb <ivar110@gmail.com>
2024-03-03 18:09:26 +0400

    Add outer html

--- frontend/angular/helloapp/src/app/app.component.ts
+++ frontend/angular/helloapp/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from "@angular/core";
-import {FormsModule} from "@angular/forms";
-     
+import { FormsModule } from "@angular/forms";
+
 @Component({
     selector: "my-app",
     standalone: true,
     imports: [FormsModule],
-    template: `<label>Введите имя:</label>
-                 <input [(ngModel)]="name" placeholder="name">
-                 <h1>Добро пожаловать {{name}}!</h1>`
+    template: `<div class="container">
+        <label>Введите имя:</label>
+        <input [(ngModel)]="name" placeholder="name">
+        <h1>Добро пожаловать {{name}}!</h1>
+    </div>`
 })
-export class AppComponent { 
-    name= "";
+export class AppComponent {
+    name = "";
 }
