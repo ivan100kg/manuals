90f2f89fa211a45ef598f95730f5c88ac13362de
IvanB <ivar110kg@gmail.com>
2024-03-04 11:29:43 +0400

    Add angular styles

--- frontend/angular/helloapp/src/app/app.component.ts
+++ frontend/angular/helloapp/src/app/app.component.ts
@@ -5,16 +5,8 @@ import { FormsModule } from "@angular/forms";
     selector: "my-app",
     standalone: true,
     imports: [FormsModule],
-    template: `<div class="container">
-        <label>Введите имя:</label>
-        <input [(ngModel)]="name" placeholder="name">
-        <h1>Добро пожаловать {{name}}!</h1>
-        <div [class.redbox]="isRed">
-            Добро пожаловать {{name}} твой возраст {{age}}!
-        </div>
-        <input [(ngModel)]="isRed">
-    </div>`,
-    styles: `.redbox{color: red;}`
+    templateUrl: './app.component.html',
+    styleUrls: ['../style.css', './app.component.css']
 })
 export class AppComponent {
     name = "";
